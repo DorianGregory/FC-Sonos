89eff9612d3d0ba2881da8d20ff4e1b794d2ed23
hashfactory <addict1@outlook.com>
2017-06-18 15:50:12 -0400

    Added functionality and descriptions

--- SonosInterface.py
+++ SonosInterface.py
@@ -13,10 +13,24 @@ class SonosInterface:
         print("find_zones called")
 
     def play(self):
+        # Plays the current track
         request("play")
 
     def pause(self):
+        # Pauses the current track
         request("pause")
+
+    def playpause(self):
+        # Toggles the play/pause of the current track
+        request("playpause")
+
+    def next(self):
+        # Plays the next song in the queue
+        request("next");
+
+    def volume(self, value):
+        # Changes the volume of selected speaker
+        request("volume/" + str(value))
 
 def build_url():
     url = "http://" + config.pi_ip + ":" + str(config.port) + "/"
@@ -26,5 +40,4 @@ def request(command):
     url = build_url()
     url += config.default_speaker + "/"
     url += command
-    print(url)
     urllib2.urlopen(url)
