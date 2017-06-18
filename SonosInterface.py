cadc89d0e3228076ba459902cf0e7fe734c4bcd4
DorianGregory <dorian.charpentier@gmail.com>
2017-06-18 14:45:20 -0400

    comments

---
+++ SonosInterface.py
@@ -0,0 +1,10 @@
+
+
+# used to control speakers
+class SonosInterface:
+    def __init__():
+        self.find_zones()
+
+    # binds players
+    def find_zones():
+        0
---
+++ SpotifyInterface.py
@@ -0,0 +1,6 @@
+
+
+# used to search for songs, artists, albums, playlists
+class SpotifyInterface:
+    def __init__(self):
+        0
